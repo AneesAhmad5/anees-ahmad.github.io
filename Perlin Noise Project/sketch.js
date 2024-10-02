66c84bb62d8492d1a1e3bbb8e55372dea32956a8
AneesAhmad5 <6800588@spsd.sk.ca>
2024-10-02 15:19:38 -0600

    Updated the Perlin Noise assignment

--- Perlin Noise Project/sketch.js
+++ Perlin Noise Project/sketch.js
@@ -12,65 +12,57 @@ function setup() {
 }
 
 // Perlin noise terrain generator.
-let cTime = 5; //current "noise" time
-let cInterval = 0.002; //how fast we move down the noise graph
-
-function perlinTerrain(){
-  let tSize = noise(cTime); 
-  tSize = map(tSize, 0, 1, 1, 255)
-  for(let x = 0; x<width; x+= 5){
-   //                         ^rectangle spacing
-    fill(0)
-    // Making the rectangles
-    let rectHeight = tSize
-    rect(x, height-20, 5, rectHeight)
-  }
-}
-
-
-function perlinCircle() {
-  //draw a circle using noise() with
-  //unpredictable size (later, color)
-  let cSize = noise(cTime); // 0 - 1
-  cSize = map(cSize, 0, 1, 1, 255);
-  
-  let r = noise(cTime); 
-  let g = noise(cTime + 2);
-  let b = noise(cTime + 5);  
-
-  r = map(r, 0, 1, 0, 255);
-  g = map(g, 0, 1, 0, 255);
-  b = map(b, 0, 1, 0, 255);
-
-  fill(r,g,b);
-  circle(width * 0.5, height / 2, cSize);
-  text(round(cSize), width * 0.5, height / 4);
-  cTime += cInterval;
-}
-
+// let cTime = 5; //current "noise" time
+let cInterval = 0.01; //how fast we move down the noise graph
 
 //Drawing function where elements come into play
 function draw() {
   background(220);
-  // staircase()
-  //perlinTerrain()
-  test()
+  generateTerrain();
+  frameRate()
+
+}
+
+let tWidth = 10
+let start = 0
+function generateTerrain(){
+  let maxY = 10
+  let maxX = 0
+  stroke(0);
+  noFill();
+  let time = 0   //start
+  for(let x = 0; x < width; x+=tWidth) {
+    stroke(0)
+    let y = noise(time) * height;
+    if(y>maxY){
+      maxY = y;
+      maxX = x; //*** */
+      drawFlag(maxX,maxY);
+    }
+    rect(x,height,tWidth,-y);
+    time += cInterval;
+  }
+  // start += cInterval;
+}
+
+function keyReleased(){
+  if(keyCode === LEFT_ARROW){
+    if (tWidth > 0.001){ 
+      console.log("-tWidth");
+      tWidth = tWidth - 1;
+    } 
+  }
+  if(keyCode === RIGHT_ARROW){
+    if (tWidth < 20){
+      console.log("+tWidth");
+      tWidth = tWidth + 1;
+    }
+  }
 }
 
 
-let start = 0
-function test(){
+function drawFlag(x, y){
   stroke(0);
-  noFill();
-  beginShape();
-  let time = start
-  for(let x = 0; x < width; x++) {
-    stroke(0)
-    let y = noise(time) * height;
-    vertex(x,y);
-
-    time += cInterval
-  }
-  endShape();
-  start += cInterval
+  line(x,y,x,y-40)
+  triangle(x,y-40,x,y-20,x+20,y-30)
 }
